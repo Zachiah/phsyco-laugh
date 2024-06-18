220827074305b907d8d10a634977aa2e5603addf
Zachiah Sawyer <zachiah@proton.me>
2024-06-17 19:00:24 -0700

    add help menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 
 	let laugh = '';
 	let alert = false;
+	let help = false;
 </script>
 
 <svelte:head>
@@ -14,9 +15,16 @@
 		class="mx-auto my-32 flex max-w-prose flex-col gap-4 rounded-md bg-pink-400 p-8 shadow-md shadow-pink-400"
 	>
 		<h1
-			class="relative font-cursive text-4xl after:absolute after:-bottom-2 after:left-0 after:h-6 after:w-full after:rounded-md after:bg-purple-200 after:opacity-40 after:content-['']"
+			class="relative flex font-cursive text-4xl after:absolute after:-bottom-2 after:left-0 after:h-6 after:w-full after:rounded-md after:bg-purple-200 after:opacity-40 after:content-['']"
 		>
 			Psycho Laugh Generator
+			<button
+				on:click={() => {
+					help = true;
+				}}
+				class="ml-auto flex h-8 w-8 items-center justify-center rounded-full bg-white text-2xl"
+				>?</button
+			>
 		</h1>
 		{#if laugh}
 			<button
@@ -64,5 +72,31 @@
 		class="fixed bottom-4 left-4 right-4 rounded-md bg-purple-200 p-4 shadow-md shadow-purple-200"
 	>
 		Copied to clipboard
+	</div>
+{/if}
+
+{#if help}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		transition:fade
+		on:click={(e) => {
+			if (e.currentTarget === e.target) {
+				help = false;
+			}
+		}}
+		class="fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col items-center bg-black bg-opacity-40 p-4 pt-32"
+	>
+		<div class="w-full max-w-prose rounded-md bg-white p-4 flex flex-col gap-4">
+			<p class="bg-purple-200 rounded-md p-4">Click the button labeled "Generate ✨" to generate your randomized psycho laugh.</p>
+			<p class="bg-purple-200 rounded-md p-4">Click on the psycho laugh that was generated to copy it to clipboard</p>
+			<p class="bg-purple-200 rounded-md p-4">Click the "?" button to show this help</p>
+			<button
+				class="w-min ml-auto rounded-md bg-pink-400 px-4 py-2"
+				on:click={() => {
+					help = false;
+				}}>Close</button
+			>
+		</div>
 	</div>
 {/if}
